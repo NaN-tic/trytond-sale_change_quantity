818aede3cb8131d5d8de7617ea0a333f1a35c105
Raimon Esteve <raimon@nan-tic.com>
2018-05-11 10:12:37 +0200

    Register classes

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .sale import *
+from . import sale
 
 
 def register():
     Pool.register(
-        Sale,
-        SaleLine,
-        ChangeLineQuantityStart,
+        sale.Sale,
+        sale.SaleLine,
+        sale.ChangeLineQuantityStart,
         module='sale_change_quantity', type_='model')
     Pool.register(
-        ChangeLineQuantity,
+        sale.ChangeLineQuantity,
         module='sale_change_quantity', type_='wizard')
